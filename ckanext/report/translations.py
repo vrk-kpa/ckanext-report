a53c2775534c2c0421afccfca8914ec520a464ac
Aaron Hakala <aaron.hakala@gmail.com>
2019-07-31 14:26:40 +0300

    added translation string to transifex

---
+++ ckanext/report/translations.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def _translations():
+    """ Does nothing but hints message extractor to extract missing strings. """
+    from ckan.common import _
+
+    _('Score frequencies')
+    _('Datasets given a score')
+    _('Score TBC')
+    _('Score')
+    _('Total stars')
+    _('Average score')
+    _('Broken datasets')
+    _('% Broken links')
