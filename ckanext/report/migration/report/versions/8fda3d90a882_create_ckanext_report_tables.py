5abaa9ee5d85a66afc1a2f69e95e7f9d5d2e3a7c
Teemu Erkkola <teemu.erkkola@gofore.com>
2026-01-26 15:11:04 +0200

    CKAN 2.11.4 compatibility

---
+++ ckanext/report/migration/report/versions/8fda3d90a882_create_ckanext_report_tables.py
@@ -0,0 +1,41 @@
+"""Create ckanext-report tables
+
+Revision ID: 8fda3d90a882
+Revises: 
+Create Date: 2026-01-09 12:58:09.160088
+
+"""
+from alembic import op
+from uuid import uuid4
+import sqlalchemy as sa
+import datetime
+
+
+# revision identifiers, used by Alembic.
+revision = '8fda3d90a882'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    engine = op.get_bind()
+    inspector = sa.inspect(engine)
+    tables = inspector.get_table_names()
+
+    if "data_cache" not in tables:
+        op.create_table(
+            "data_cache",
+            sa.Column("id", sa.UnicodeText, primary_key=True, default=uuid4),
+            sa.Column("object_id", sa.UnicodeText, index=True),
+            sa.Column("key", sa.UnicodeText, nullable=False),
+            sa.Column("value", sa.UnicodeText),
+            sa.Column("created", sa.DateTime, default=datetime.datetime.now),
+        )
+
+    op.create_index('idx_data_cache_object_id_key', 'data_cache', ['object_id'], if_not_exists=True)
+
+
+def downgrade():
+    op.drop_table("data_cache")
+    op.drop_index('idx_data_cache_object_id_key', 'data_cache')
